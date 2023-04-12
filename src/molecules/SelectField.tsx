cef05e57571a957ade02c026de0c268a6a1bd617
samir <arrahmani.samir@gmail.com>
2023-04-12 18:54:50 +0200

    wip

---
+++ src/molecules/SelectField.tsx
@@ -0,0 +1,60 @@
+import React, { FC, InputHTMLAttributes } from "react"
+import * as hookForm from "react-hook-form"
+import { TextField } from "utils/styles/textField.style"
+
+type SelectFieldProps = {
+  control: hookForm.Control<any> | undefined
+  error: hookForm.FieldErrors
+  name: string
+  label: string
+  options: { key: string; label: string }[]
+  placeholder?: string
+  disabled?: boolean
+  helperText?: any
+  icon?: JSX.Element
+  type?: InputHTMLAttributes<unknown>["type"]
+}
+
+const SelectField: FC<SelectFieldProps> = ({
+  control,
+  helperText,
+  error,
+  name,
+  label,
+  placeholder,
+  disabled,
+  options,
+}: SelectFieldProps) => {
+  return (
+    <hookForm.Controller
+      name={name}
+      control={control}
+      defaultValue=""
+      render={({ field: { onChange, onBlur, value, ref } }) => (
+        <TextField
+          select
+          inputRef={ref}
+          disabled={disabled}
+          placeholder={placeholder}
+          value={value}
+          label={label}
+          onBlur={onBlur}
+          error={!!error[name]}
+          helperText={(error[name] && error[name]?.message) || helperText}
+          onChange={(newValue) => onChange(newValue)}
+          variant="outlined"
+          SelectProps={{ native: true }}
+        >
+          <option aria-label="None" value="" />
+          {options.map((option) => (
+            <option key={option.key} value={option.key}>
+              {option.label}
+            </option>
+          ))}
+        </TextField>
+      )}
+    />
+  )
+}
+
+export default SelectField
