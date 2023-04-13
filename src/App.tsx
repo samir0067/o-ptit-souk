94d341bc2bde2e34a30cb77c499a642cca08a340
samir <arrahmani.samir@gmail.com>
2023-04-13 08:31:58 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from "react"
-import Home from "pages/Home"
 import { Route, Routes } from "react-router-dom"
+import Home from "pages/Home"
+import SignUp from "./pages/SignUp"
+import Profiles from "pages/Profiles"
 import NotFound from "pages/NotFound"
 
 const App: FC = () => {
   return (
     <Routes key={location.pathname} location={location}>
       <Route path="/" element={<Home />} />
+      <Route path="/inscription" element={<SignUp />} />
+      <Route path="/clients" element={<Profiles />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { RecoilRoot } from "recoil"
 import App from "./App"
+import { CssBaseline } from "@mui/material"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import "@fontsource/dekko/400.css"
+import "@fontsource/poppins/400.css"
+import "@fontsource/poppins/500.css"
+import "@fontsource/poppins/700.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <RecoilRoot>
+        <CssBaseline />
         <App />
       </RecoilRoot>
     </BrowserRouter>
