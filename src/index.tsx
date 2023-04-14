ef0ba98db43d4b1499e2a5dbc7497e6d899e7076
samir <arrahmani.samir@gmail.com>
2023-04-14 23:07:18 +0200

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,6 @@ import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
 import "@fontsource/dekko/400.css"
-import "@fontsource/poppins/400.css"
-import "@fontsource/poppins/500.css"
-import "@fontsource/poppins/700.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
