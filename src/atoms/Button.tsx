98be38640ef5d240e69fc14f5cf2a96c43e29a85
samir <arrahmani.samir@gmail.com>
2023-04-14 07:08:05 +0200

    wip

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactNode } from "react"
-import { Button as MuiButton, SxProps } from "@mui/material"
+import { SxProps } from "@mui/material"
+import { LoadingButton } from "@mui/lab"
 import { colors } from "utils/constants"
 import "@fontsource/roboto/700.css"
 
@@ -12,6 +13,8 @@ type ButtonProps = {
   margin?: string
   padding?: string
   disabled?: boolean
+  loading?: boolean
+  loadingPosition?: "start" | "center" | "end"
   size?: "medium" | "small" | "large"
   variant?: "text" | "contained" | "outlined"
   backgroundColor?: string
@@ -22,20 +25,24 @@ type ButtonProps = {
 const Button: FC<ButtonProps> = ({
   label,
   onClick,
-  variant,
   icon,
   sx,
-  size,
-  margin,
-  padding,
-  disabled,
-  color,
-  backgroundColor,
-  backgroundColorHover,
+  disabled = false,
+  loading = false,
+  loadingPosition = "center",
   borderColor,
+  size = "medium",
+  margin = "10px 0",
+  padding = "7px 23px",
+  variant = "contained",
+  color = colors.white,
+  backgroundColor = colors.lightBlue,
+  backgroundColorHover = colors.lightBlue,
 }: ButtonProps) => {
   return (
-    <MuiButton
+    <LoadingButton
+      loading={loading}
+      loadingPosition={loadingPosition}
       startIcon={icon}
       size={size}
       variant={variant}
@@ -54,18 +61,8 @@ const Button: FC<ButtonProps> = ({
       }}
     >
       {label}
-    </MuiButton>
+    </LoadingButton>
   )
-}
-
-Button.defaultProps = {
-  size: "medium",
-  margin: "10px 0",
-  padding: "7px 23px",
-  variant: "contained",
-  color: colors.white,
-  backgroundColor: colors.lightBlue,
-  backgroundColorHover: colors.lightBlue,
 }
 
 export default Button
