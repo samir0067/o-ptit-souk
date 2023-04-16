3debb0db263e5c9c0cd9feb56a2c5af245d99400
samir <arrahmani.samir@gmail.com>
2023-04-16 23:08:04 +0200

    wip

--- src/organisms/home/CarouselSection.tsx
+++ src/organisms/home/CarouselSection.tsx
@@ -1,14 +1,38 @@
 import React, { FC } from "react"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 import { Box, Grid } from "@mui/material"
-import { Carousel } from "molecules/Carousel"
 import styles from "./carouselSection.module.css"
+import Slider, { Settings } from "react-slick"
+import { dataBuffets } from "../../utils/constants/dataBuffets"
+import { Title } from "../../atoms/Title"
 
 const CarouselSection: FC = () => {
+  const settings: Settings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3500,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    arrows: false,
+  }
+
   return (
-    <Box component="div" className={styles.container}>
+    <Box component="div" className={styles.containerBox}>
       <Grid container columnSpacing={{ xs: 1, md: 3 }} sx={{ zIndex: 2 }}>
         <Grid item xs={12} className={styles.gridCarousel}>
-          <Carousel />
+          <Box height="auto" width="100%">
+            <Slider {...settings}>
+              {dataBuffets.map((buffets, index: number) => (
+                <Box key={index} className={styles.containerCarousel}>
+                  <Title title={buffets.title} variant="h3" component="h2" className={styles.titleStyles} />
+                  <Box component="img" src={buffets.index} alt={buffets.name} className={styles.imgStyles} />
+                </Box>
+              ))}
+            </Slider>
+          </Box>
         </Grid>
       </Grid>
     </Box>
