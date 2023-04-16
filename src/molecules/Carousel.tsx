c8358d9f2b7d48fcfcfe46ace3c8357ff715280f
samir <arrahmani.samir@gmail.com>
2023-04-16 19:22:52 +0200

    wip

--- src/molecules/Carousel.tsx
+++ src/molecules/Carousel.tsx
@@ -3,80 +3,29 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Slider, { Settings } from "react-slick"
 import { Box } from "@mui/material"
-import useBreakpoints from "../services/hooks/useBreakpoints"
-import burgerTray from "assets/images/burgerTray.jpg"
-import cupCakesTray from "assets/images/cupCakesTray.jpg"
-import burgerTray3 from "assets/images/burgerTray3.jpg"
-import snacks from "assets/images/snacks.jpg"
-import verrines from "assets/images/verrines.jpg"
-import burgerTray2 from "assets/images/burgerTray2.jpg"
-import toast2 from "assets/images/toast2.jpg"
-import salades from "assets/images/salades.jpg"
-
-export const dataBuffet = [
-  {
-    id: 1,
-    name: "burgerTray.jpg",
-    index: burgerTray,
-  },
-  {
-    id: 2,
-    name: "cupCakesTray",
-    index: cupCakesTray,
-  },
-  {
-    id: 3,
-    name: "snacks",
-    index: snacks,
-  },
-  {
-    id: 4,
-    name: "burgerTray2",
-    index: burgerTray2,
-  },
-  {
-    id: 5,
-    name: "verrines",
-    index: verrines,
-  },
-  {
-    id: 6,
-    name: "burgerTray3",
-    index: burgerTray3,
-  },
-  {
-    id: 7,
-    name: "toast2",
-    index: toast2,
-  },
-  {
-    id: 8,
-    name: "salades",
-    index: salades,
-  },
-]
+import { Title } from "../atoms/Title"
+import { dataBuffets } from "../utils/constants/dataBuffets"
+import styles from "./carousel.module.css"
 
 export const Carousel: FC = () => {
-  const { downMd } = useBreakpoints()
-
   const settings: Settings = {
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     speed: 3500,
     autoplaySpeed: 3000,
     pauseOnHover: true,
     arrows: false,
-    vertical: downMd,
   }
 
   return (
     <Box height="auto" width="100%">
       <Slider {...settings}>
-        {dataBuffet.map((slideTrustUs, index: number) => (
-          <Box key={index} height="7rem">
-            <Box component="img" src={slideTrustUs.index} alt={slideTrustUs.name} m="auto" height="6rem" />
+        {dataBuffets.map((buffets, index: number) => (
+          <Box key={index} className={styles.container}>
+            <Title title={buffets.title} variant="h3" component="h2" className={styles.titleStyles} />
+            <Box component="img" src={buffets.index} alt={buffets.name} className={styles.imgStyles} />
           </Box>
         ))}
       </Slider>
