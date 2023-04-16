8bc7d8f10353fe878c932ce588f0e57083fad96f
samir <arrahmani.samir@gmail.com>
2023-04-16 20:16:23 +0200

    wip

---
+++ src/utils/constants/constants.ts
@@ -0,0 +1,10 @@
+export const colorsBis = {
+  extraLightNavy: "#c9ede8",
+  lightNavy: "#57C5B6",
+  mediumNavy: "#159895",
+  normalNavy: "#1A5F7A",
+  darkNavy: "#002B5B",
+  black: "#010400",
+  white: "#FFFFFA",
+  transparent: "rgba(0,0,0,0)",
+}
---
+++ src/utils/constants/dataBuffets.ts
@@ -0,0 +1,59 @@
+import burgerTray from "../../assets/images/burgerTray.jpg"
+import cupCakesTray from "../../assets/images/cupCakesTray.jpg"
+import snacks from "../../assets/images/snacks.jpg"
+import burgerTray2 from "../../assets/images/burgerTray2.jpg"
+import verrines from "../../assets/images/verrines.jpg"
+import burgerTray3 from "../../assets/images/burgerTray3.jpg"
+import toast2 from "../../assets/images/toast2.jpg"
+import salades from "../../assets/images/salades.jpg"
+
+export const dataBuffets = [
+  {
+    id: 1,
+    name: "burgerTray.jpg",
+    title: "Des produits de qualité, préparés avec soin et passion.",
+    index: burgerTray,
+  },
+  {
+    id: 2,
+    name: "cupCakesTray",
+    title: "Une large gamme de délices sucrés et salés pour tous les goûts.",
+    index: cupCakesTray,
+  },
+  {
+    id: 3,
+    name: "snacks",
+    title: "Quel que soit le type d'événement que vous organisez, \nous avons une solution adaptée à vos besoins",
+    index: snacks,
+  },
+  {
+    id: 4,
+    name: "burgerTray2",
+    title: "Des options pour tous les régimes alimentaires, \ny compris végétarien, végétalien et sans gluten.",
+    index: burgerTray2,
+  },
+  {
+    id: 5,
+    name: "verrines",
+    title: "N'attendez plus pour faire de votre événement un moment inoubliable.",
+    index: verrines,
+  },
+  {
+    id: 6,
+    name: "burgerTray3",
+    title: "Des options pour tous les régimes alimentaires, \n y compris végétarien, végétalien et sans gluten.",
+    index: burgerTray3,
+  },
+  {
+    id: 7,
+    name: "toast2",
+    title: "des fournisseurs locaux et responsables pour \n assurer que nos ingrédients sont de la meilleure qualité",
+    index: toast2,
+  },
+  {
+    id: 8,
+    name: "salades",
+    title: "Parcourez notre site pour découvrir notre offre alléchante et n'hésitez plus.",
+    index: salades,
+  },
+]
