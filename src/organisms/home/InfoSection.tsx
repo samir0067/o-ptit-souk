17653786b690443637abe438d5f7cdccd839511a
samir <arrahmani.samir@gmail.com>
2023-04-16 18:35:52 +0200

    wip

--- src/organisms/home/InfoSection.tsx
+++ src/organisms/home/InfoSection.tsx
@@ -1,31 +1,46 @@
 import React, { FC } from "react"
-import { Box, Grid, SxProps } from "@mui/material"
+import { Box, Grid } from "@mui/material"
 import styles from "./infoSection.module.css"
 import Card from "../../molecules/Card"
 import { useNavigate } from "react-router-dom"
+import { colors } from "../../utils/constants"
+import aperitif2 from "assets/images/aperitif2.jpg"
+import burgerTray2 from "assets/images/burgerTray2.jpg"
+import logoBackBlack from "assets/logoBackBlack.jpg"
 
 const InfoSection: FC = () => {
   const navigate = useNavigate()
 
-  const gridTitles: SxProps = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    flexDirection: "column",
-    height: "75vh",
-  }
-
   return (
-    <Box component="div" className={styles.backgroundImage}>
-      <Grid container columnSpacing={{ xs: 1, md: 3 }} sx={{ zIndex: 2 }}>
-        <Grid item xs={12} sx={gridTitles}>
-          <Card
-            title={content.title}
-            description={content.description}
-            btnLabel={content.btnCardLabel}
-            btnOnClick={() => navigate("/carte")}
-          />
-        </Grid>
+    <Box component="div" className={styles.containerImage}>
+      <Grid container spacing={{ xs: 1, md: 3 }} className={styles.gridContainer}>
+        <Card
+          img={aperitif2}
+          title={content.title}
+          backgroundColor={colors.lightCoral}
+          btnLabel={content.btnCardLabel}
+          btnBackgroundColor={colors.darkCoral}
+          btnOnClick={() => navigate("/carte")}
+        />
+        <Card
+          img={logoBackBlack}
+          title={content.title2}
+          titleColor={colors.white}
+          btnColor={colors.black}
+          btnBackgroundColor={colors.lightCoral}
+          btnLabel={content.btnCardLabel}
+          btnOnClick={() => navigate("/carte")}
+          backgroundColor={colors.darkCoral}
+          gridClassName={styles.centerCard}
+        />
+        <Card
+          img={burgerTray2}
+          title={content.title3}
+          btnLabel={content.btnCardLabel}
+          btnOnClick={() => navigate("/carte")}
+          backgroundColor={colors.lightCoral}
+          btnBackgroundColor={colors.darkCoral}
+        />
       </Grid>
     </Box>
   )
@@ -35,8 +50,9 @@ const content = {
   headerTitle: "Ô Ptit Souk",
   title:
     "Bienvenue Chez Ô Ptit Souk, votre traiteur en ligne spécialisé en délices sucrés et salés pour tous vos événements !",
-  description:
-    "Nous sommes passionnés par la création de saveurs uniques et la satisfaction. Nous vous proposons une large gamme de produits sucrés et salés, préparés avec soin à partir d'ingrédients de qualité et toujours frais.",
+  title2: "Passionnés par la création de saveurs uniques et la satisfaction.",
+  title3:
+    "Avec une large gamme de produits sucrés et salés, préparés avec soin à partir d'ingrédients de qualité et toujours frais.",
   btnCardLabel: "Visiter la carte",
 }
 export default InfoSection
