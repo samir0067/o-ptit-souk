27c3756b1e895cfc275cf04fa748e9787ee5871d
samir <arrahmani.samir@gmail.com>
2023-04-19 08:40:01 +0200

    wip

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from "react"
 import { Box } from "@mui/material"
-import { useRecoilValue } from "recoil"
-import { todoListState } from "../services/store/atoms"
 import HeaderSection from "../organisms/home/HeaderSection"
 import InfoSection from "../organisms/home/InfoSection"
 import CarouselSection from "../organisms/home/CarouselSection"
@@ -9,9 +7,6 @@ import CatererSection from "../organisms/home/CatererSection"
 import Footer from "../molecules/Footer"
 
 const Home: FC = () => {
-  const todoList = useRecoilValue(todoListState)
-  console.log("todoList =>", todoList)
-
   return (
     <Box component="div">
       <HeaderSection />
