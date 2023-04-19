7c6b3bb87017c81e6b9c22e8a30c4ebc9a157077
samir <arrahmani.samir@gmail.com>
2023-04-19 08:40:33 +0200

    wip

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,7 +1,10 @@
 export const colorsBis = {
   extraLightNavy: "#c9ede8",
+  extraLightPink: "#FBD7E0",
   lightNavy: "#57C5B6",
+  lightPink: "#F8B0C1",
   mediumNavy: "#159895",
+  mediumPink: "#EF476F",
   normalNavy: "#1A5F7A",
   darkNavy: "#002B5B",
   black: "#010400",
